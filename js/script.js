b8b65200a4e7484d255d811ac75de52b89200786
Ahmed Abd El-Halim <a.halim15@hotmail.com>
2024-04-20 13:40:01 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -94,9 +94,7 @@ document.getElementById("searchInput").addEventListener("keyup", function () {
     search(); 
 });
 
-let CairoTimeStop = setInterval(function() {
-    getWeatherData("cairo");
-}, 1000);
+let CairoTimeStop = getWeatherData("cairo");
 
 function search() {
     let q = document.getElementById("searchInput").value;  
@@ -109,3 +107,30 @@ function search() {
     getNextDays(q);     
 }
 
+
+
+
+
+function rain() {
+    let cloud = document.querySelector('.cloud');
+    let e = document.createElement('div');
+    let left = Math.floor(Math.random() * 310);
+    let width = Math.random() * 5;
+    let height = Math.random() * 50;
+    let duration = Math.random() * 0.5;
+
+    e.classList.add('drop');
+    cloud.appendChild(e);
+    e.style.left = left + 'px';
+    e.style.width = 0.5 + width + 'px';
+    e.style.height = 0.5 + height + 'px';
+    e.style.animationDuration = 1+duration+'s';
+
+    setTimeout(function () {
+        cloud.removeChild(e);
+    }, 2000);
+}
+
+setInterval(function(){
+rain()
+},20);
